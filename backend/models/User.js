df00a50c84bcb2e94d626ae6be100b1198db1603
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2021-03-08 23:51:53 +0100

    commiting changes from the user.js and server.js files

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,required:true,
+    },
+
+    email: {
+        type: String, required: true,
+        unique:true,
+    },
+
+    password: {
+        type: String,required:true,
+    },
+    isAdmin: {
+        type: Boolean, required: true,
+        default: false,
+    },
+},
+    {timestamps: true}
+);
+
+userSchema.methods.matchPassword = async((enteredpassword) =>
+{
+    return await bcrypt.compare(enteredpassword, this.password);
+});
+
+userSchema.pre('save,', async (next) =>
+{
+    if (!this.isModified('password'))
+    {
+        next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+});
