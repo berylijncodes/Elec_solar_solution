0efec2cfd55c658051ed5eb2ab5549789e819c70
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2021-03-08 20:23:19 +0100

    changes from the package.json and server.js file

--- frontend/src/screen/HomeScreen/index.js
+++ frontend/src/screen/HomeScreen/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+//assests
+import classes from './HomeScreen.module.css'
+import FemiCare from './assests/images/FemiCare.png';
+import Battery002 from './assests/images/Battery002.jpg';
 
-const HomeScreen = ()=> {
+
+const HomeScreen = () =>
+{
     return (
-        <div>
-            
+        <div className={classes.screen_container}>
+        
+            <div className={classes.slide}>
+                <div className={classes.greySqaure}>
+
+                    <h1 className={classes.title}>Ruby Sunday</h1>
+                    <h1 className = {classes.title}>Photography</h1>
+                </div>
+            <img className = {classes.slide_image} src = {FemiCare} alt = 'FemiCare'/>
+
+            </div>
         </div>
     )
 }
