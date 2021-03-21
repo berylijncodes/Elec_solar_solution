d3370d7bc2d16df63d65a1ec739de152c9ff4d8e
Sunday Abam <abamsunday5@gmail.comgit config --edit --globalcodemkdird>
2021-03-21 20:37:14 +0100

    commiting changes from .env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async  () => {
     try
     {
-        await mongoose.connect(process.env.MONGO_URI,{
+        await mongoose.connect(process.env.MONGO_DB_CONNECTION_URL,{
             useNewUrlParser: true,
             useUnifiedTopology : true,
             useFindAndModify: false,
